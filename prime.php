23441f9ec2fc0af9c498375cc168185f6f3ed7f2
balamurugan301118008 <balamuruganrdckap@gmail.com>
2023-05-19 15:42:09 +0530

    completed the prime number problem

--- prime.php
+++ prime.php
@@ -1,18 +1,25 @@
 <?php
 function primeNumber($num)
 {
-    $check = 0;
-    for ($i = 2; $i <= $num-1; $i++) {
+
+    if($num > 0){
+        $check = 0;
+    for ($i = 2; $i <= $num - 1; $i++) {
         if ($num % $i == 0) {
             $check = 1;
             break;
         }
     }
     if ($check == 0) {
-        echo "$num.is a Prime Number";
+        echo "$num is a Prime Number"."\n";
     } else {
-        echo "$num.is not a  Prime Number";
+        echo "$num is not a  Prime Number"."\n";
     }
+    }
+    else{
+        echo "$num empty value so please put greater 0 and 1 values"."\n";
+    }
+    
 }
 echo primeNumber(11);
 
